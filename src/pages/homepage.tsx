9838ab3af6f90ac4d6f9678df0d38cd1063a8e03
BijesniPatuljak <dario.bogovic@thespian.hr>
2023-12-27 20:34:24 +0100

    Color name changes, homepage content

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -11,6 +11,7 @@ function Homepage() {
   const openNavigation = useSelector((state: RootState) => state.navigation.value);
   const className = openNavigation ? "page-wrapper open-navigation" : "page-wrapper";
   const dispatch = useDispatch();
+  const since2012 = new Date().getFullYear() - 2012;
 
   const handleOnClick = () => {
     if (!openNavigation) return;
@@ -21,8 +22,15 @@ function Homepage() {
     <div className={className} onClick={handleOnClick}>
       <div className='overlay'>
         <Pointer />
-        <div className="title">Homepage</div>
-        <div className="content">content</div>
+        <div className="title">Here lives a Scout</div>
+        <div className="content">
+          <p className='heading'>
+            But you're probably not here to listen about my journeys as a Scout and Scoutmaster
+          </p>
+          <p>
+            Something has brought you here to check out my skills and knowledge regarding software development. The first 'Hi' I ever got from a small terminal window (which happened by my own doing) was back in 2012. A line of code lead to another and here I am {since2012} years later. But without further ado, click on the navigation menu and let's get you up to speed!
+          </p>
+        </div>
       </div>
     </div>
   );

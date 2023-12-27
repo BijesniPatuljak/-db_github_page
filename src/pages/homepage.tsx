48572cb8a4fd8239896906c477dbe1fc3560291d
BijesniPatuljak <dario.bogovic@thespian.hr>
2023-12-27 20:56:59 +0100

    Text styling changes

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -28,7 +28,7 @@ function Homepage() {
             But you're probably not here to listen about my journeys as a Scout and Scoutmaster
           </p>
           <p>
-            Something has brought you here to check out my skills and knowledge regarding software development. The first 'Hi' I ever got from a small terminal window (which happened by my own doing) was back in 2012. A line of code lead to another and here I am {since2012} years later. But without further ado, click on the navigation menu and let's get you up to speed!
+            Something has brought you here to check out my skills and knowledge regarding <span className='highlight'>software development</span>. The first 'Hi' I ever got from a small terminal window (which happened by my own doing) was back in <span className='highlight'>2012</span>. A line of code lead to another and here I am <span className='highlight'>{since2012}</span> years later. But without further ado, click on the <span className='highlight'>navigation</span> menu and let's get you up to speed!
           </p>
         </div>
       </div>
